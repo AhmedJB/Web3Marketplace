33b76a8dd09755f4f70ca048f33ea6caae6833c9
sky.red2212@gmail.com <Blockstacksdottech>
2023-06-26 16:46:49 +0100

    started structure for testimonies section

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import styles from "../styles/modular/Home.module.css"
+import TestComponent from '../components/TestComponent'
+import carImage from "../assets/demo/car.jpg";
+import Link from 'next/link';
+import Testimonies from '../components/Testimonies';
 
 type Props = {}
 
 function home({}: Props) {
-  return (
-    <div className={styles["home-title"]}>home</div>
-  )
+  return <>
+    <Testimonies />
+  </>
+
 }
+
+
+
+/*
+  <h1 className={`${styles.test} inter text-headerColor`}>Hello</h1>
+        <h3 className="barlow text-headerColor">hello 2</h3>
+        <p className="text-headerColor">this is a paragraph</p>
+*/
 
 export default home
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      mainDark: "#0A0A0A",
+      greenOrb: "#0D342E",
+      redOrb: "#61322D",
+      headerColor: "#D3CBB8",
+      blue: "#188CB7",
+      red: "#AA3A38",
+      green: "#2F7336",
+      yellow: "#F0D697",
+      white: "#ffffff",
+    },
     extend: {},
   },
   plugins: [],
