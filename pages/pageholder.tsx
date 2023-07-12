8f990e4d19cc1b97ba547c001042264dd6dd136a
issamelotmani <issammalika2001@gmail.com>
2023-07-12 15:46:25 +0100

    Chart Component

--- pages/pageholder.tsx
+++ pages/pageholder.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import dynamic from 'next/dynamic';
 import MenuLayout from '../components/MenuLayout';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -6,7 +7,12 @@ import ProfileComponent from '../components/ProfileComponent';
 import FavoritesComponent from '../components/FavoritesComponent';
 import DashboardComponent from '../components/DashboardComponent';
 import PaymentSuccess from '../components/PaymentSuccess';
+import ChartComponent from '../components/ChartComponent';
 
+const DynamicChartComponent = dynamic(
+  () => import('../components/ChartComponent/ChartComponentOnly'),
+  { ssr: false } // Disable server-side rendering
+);
 
 const pageholder = () => {
   const [activeMenuItem, setActiveMenuItem] = useState('profile');
@@ -18,7 +24,7 @@ const pageholder = () => {
   const getPageComponent = () => {
     switch (activeMenuItem) {
       case 'profile':
-        return <ProfileComponent/>;
+        return <ProfileComponent />;
       case 'favorites':
         return <FavoritesComponent />;
       case 'dashboard':
@@ -33,14 +39,24 @@ const pageholder = () => {
       <Header />
       <div className="w-full relative mb-64 mt-20">
         <div className="w-full md:container mx-auto">
-            <div className='flex  gap-8 pb-11'> 
-                <MenuLayout activeMenuItem={activeMenuItem} handleMenuClick={handleMenuClick} />
-                {getPageComponent()}
-            </div> 
+          <div className="flex  gap-8 pb-11">
+            <MenuLayout activeMenuItem={activeMenuItem} handleMenuClick={handleMenuClick} />
+            {getPageComponent()}
+          </div>
 
-            <div className="flex items-center justify-center py-10">
-              <PaymentSuccess />
-            </div> 
+          <div className="flex items-center justify-center py-10">
+            <PaymentSuccess />
+          </div>
+
+
+          <div>
+          <ChartComponent/>
+          </div>
+
+         
+
+
+
         </div>
       </div>
       <Footer />
