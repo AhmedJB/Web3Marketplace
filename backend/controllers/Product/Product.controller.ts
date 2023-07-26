144e45971966b16bd0d25a7f5409fcd04bc2d3f3
sky.red2212@gmail.com <Blockstacksdottech>
2023-07-26 17:02:36 +0100

    product creation endpoint with image support

---
+++ backend/controllers/Product/Product.controller.ts
@@ -0,0 +1,110 @@
+import { Router } from 'express'
+import { ServerRequestException } from '~~/utils/exceptions'
+import { db } from '~/index';
+import { PrismaClientValidationError } from '@prisma/client/runtime/library';
+import { handleCreateMissingError } from '~~/utils/prismaHandler';
+import { upload } from '~~/configurations/multerConfig';
+import { verifySignature } from '~~/utils/helper';
+import { ProductT } from '~~/types/Product';
+
+
+
+
+const ProductController = Router();
+
+let numberFields = ["shippingCost", "minimumDeliveryTime", "maximumDeliveryTime", "Price", "quantity"]
+
+
+
+/**
+ * need category model
+ */
+ProductController.post("/create/:address", upload.array("image", 4), async (req, res, next) => {
+    try {
+        const add = req.params.address
+        console.log("address ", add)
+        let body: { signature: string } = req.body
+        let res_sig = verifySignature(add, body.signature);
+        if (!res_sig) {
+            let files = req.files as Express.Multer.File[];
+            console.log(files)
+            let body = req.body as ProductT;
+            let { signature, category, ...newBody } = body
+            console.log(body);
+            for (let key of numberFields) {
+                (newBody as any)[key as keyof typeof newBody] = Number(newBody[key as keyof typeof newBody]);
+            }
+            (newBody as any).isAuction = (newBody.isAuction as unknown as string) === "true"
+            console.log(newBody)
+            //return res.status(200).json({ test: true })
+
+            const userData = await db.userModel.findUnique({
+                where:
+                    { address: add }
+            }).catch(
+                (err: PrismaClientValidationError) => {
+                    throw new ServerRequestException(handleCreateMissingError(err.message))
+                }
+            );
+            console.log(userData)
+            if (!userData) {
+                throw new ServerRequestException("User Not found");
+            } else {
+                // generating images array 
+                let im_array = files?.map((e, i) => {
+                    let relative_path = "/uploads/" + e.filename;
+                    let url = "/images/" + e.filename
+                    let temp = {
+                        filename: e.filename,
+                        physicalPath: relative_path,
+                        fileUrl: url
+                    }
+                    return temp
+                })
+                // create products
+                let userAndProduct = await db.userModel.update({
+                    where: {
+                        id: userData?.id
+                    },
+                    data: {
+                        products: {
+                            create: [
+                                {
+                                    ...newBody,
+                                    images: {
+                                        create: im_array
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }).catch(
+                    (err: PrismaClientValidationError) => {
+                        throw new ServerRequestException(handleCreateMissingError(err.message))
+                    });
+                return res
+                    .status(201)
+                    .json({
+                        created: true
+                    })
+            }
+
+        } else {
+            throw new ServerRequestException("Signature Verification Failed");
+        }
+
+    } catch (err: any) {
+        console.log(err)
+        next(err)
+    }
+
+})
+
+
+
+
+
+
+
+
+export { ProductController }
--- bazart/components/ProductCreation/UploadForm/ImageUpload/index.tsx
+++ bazart/components/ProductCreation/UploadForm/ImageUpload/index.tsx
@@ -48,12 +48,12 @@ function ImageUpload({ images, setImages }: Props) {
             <p className="text-md opacity-75 text-white font-light my-3">
                 Upload high-quality images of your product. Showcase your product from multiple angles and in various settings if possible.
             </p>
-            <input onChange={handleFileInputChange} ref={inputRef} hidden={true} type="file" accept=".png, .gif, .webp" max-size="2000000" />
+            <input onChange={handleFileInputChange} ref={inputRef} hidden={true} type="file" accept=".png, .gif, .webp, .jpg, .jpeg" max-size="4000000" />
             <div className="w-full aspect-[3/2] border-dashed border-cardGray rounded-xl border-2 grid place-items-center">
                 <div className="flex flex-col items-center gap-4">
                     <MdUploadFile className="text-white text-4xl" />
                     <p className="text-white text-md font-normal">
-                        PNG, GIF, WEBP. Max size: 2MB (Max : 4)
+                        JPG, PNG, GIF, WEBP. Max size: 4MB (Max : 4)
                     </p>
                     {
                         images.length < 4 && <button className="px-7 py-2 rounded-[3rem] text-green border-2 border-green text-sm" onClick={() => { startUpload() }}>Upload</button>
